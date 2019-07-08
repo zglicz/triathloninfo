2f9695dc52382b57d8199cd8c6730a99f8d24ac1
Michal Zgliczynski <zglicz@fb.com>
2019-07-08 16:54:23 +0200

    Add all+both options to age-group and sex

--- ironman/forms.py
+++ ironman/forms.py
@@ -2,14 +2,20 @@ from django import forms
 from .models import Race, RaceResult
 
 class RaceFilterForm(forms.Form):
-	race_id = forms.ModelChoiceField(queryset=Race.objects.none(), widget=forms.Select(attrs={"onChange": 'submit()'}))
+	race_id = forms.ModelChoiceField(
+		queryset=Race.objects.none(),
+		widget=forms.Select(attrs={"onChange": 'submit()', 'class': 'form-control'}),
+		label='',
+		empty_label=None)
 
 	age_group = forms.ChoiceField(
-		choices=RaceResult.AGE_GROUPS,
-		widget=forms.Select(attrs={'onchange': 'submit();'}))
+		choices=[(None, 'All')] + RaceResult.AGE_GROUPS,
+		widget=forms.Select(attrs={'onchange': 'submit();', 'class': 'form-control'}),
+		label='')
 	sex = forms.ChoiceField(
-		choices=RaceResult.SEXES,
-		widget=forms.Select(attrs={'onchange': 'submit();'}))
+		choices=[(None, 'Both')] + RaceResult.SEXES,
+		widget=forms.Select(attrs={'onchange': 'submit();', 'class': 'form-control'}),
+		label='')
 
 	def __init__(self, race, *args, **kwargs):
 		super(RaceFilterForm, self).__init__(*args, **kwargs)
